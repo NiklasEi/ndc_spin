169aa513b122c81afc36d74b93d7c1f6fb4421ba
Niklas Eicker <git@nikl.me>
2024-06-15 17:31:03 +0200

    Render and template with given input

--- render/src/lib.rs
+++ render/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use spin_sdk::http::{IntoResponse, Request, Response};
 use spin_sdk::http_component;
 use templating::render_template;
@@ -8,29 +9,20 @@ use typst::foundations::Smart;
 #[http_component]
 fn handle_render(req: Request) -> anyhow::Result<impl IntoResponse> {
     println!("Handling request to {:?}", req.header("spin-full-url"));
-    let input = "{
-  \"test\": \"from sample data\",
-  \"items\": [
-    {
-      \"name\": \"Frank\",
-      \"one\": \"first\",
-      \"two\": \"second\",
-      \"three\": \"third\"
-    },
-    {
-      \"name\": \"John\",
-      \"one\": \"first_john\",
-      \"two\": \"second_john\",
-      \"three\": \"third_john\"
-    }
-  ]
-}";
+    let input = serde_json::from_reader::<_, serde_json::Value>(req.body())
+        .context("Failed to parse input as json")?;
+    let template = req
+        .header("spin-path-match-id")
+        .context("Failed to get template name from request path")?
+        .as_str()
+        .context("Failed to convert template id to str")?;
+
     let document = render_template(
-        "test".to_string(),
+        template.to_string(),
         input.to_string(),
-        WasiTemplateFiles::new("templates", "test"),
+        WasiTemplateFiles::new("templates", template),
     )
-    .expect("Failed to render");
+    .context("Failed to render template")?;
     let pdf_buffer = typst_pdf::pdf(&document, Smart::Auto, None);
     Ok(Response::builder()
         .status(200)
